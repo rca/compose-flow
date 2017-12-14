026cc7e1654be6a49e49c15c9d4a165b7028804f
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2017-12-14 12:41:43 -0500

    Add setup.py and manifest file

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='dc-workflows',
+    url='https://github.com/rca/dc',
+    author='Roberto Aguilar',
+    author_email='roberto.c.aguilar@gmail.com',
+    version='0.0.1',
+    description='codified workflows for docker compose',
+    long_description=readme(),
+    scripts=['dc'],
+    install_requires=[
+        'PyYAML',
+        'sh',
+    ]
+)
