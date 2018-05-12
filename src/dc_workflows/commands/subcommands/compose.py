fd0a6d10fa306446969848c69cfb598887e8da58
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-05-11 23:52:59 -0400

    Add compose command

---
+++ src/dc_workflows/commands/subcommands/compose.py
@@ -0,0 +1,62 @@
+"""
+Compose subcommand
+"""
+import argparse
+import logging
+import os
+import sys
+
+import sh
+
+from .base import BaseSubcommand
+from .env import Env
+from .profile import Profile
+
+from dc_workflows import docker
+
+from distutils.spawn import find_executable
+
+
+class Compose(BaseSubcommand):
+    """
+    Subcommand for running compose commands
+    """
+    @classmethod
+    def fill_subparser(cls, parser, subparser) -> None:
+        subparser.add_argument('docker_args', nargs=argparse.REMAINDER)
+
+    def handle(self):
+        profile = Profile(self.workflow)
+        profile.write()
+
+        command = ['docker-compose']
+        command[0] = find_executable(command[0])
+
+        command.extend([
+            '--project-name', self.args.project_name,
+            '-f', profile.filename,
+        ])
+
+        command.extend(self.args.docker_args)
+
+        self.logger.info(' '.join(command))
+
+        if not self.args.dry_run:
+            # os.execve(command[0], command, os.environ)
+            proc = getattr(sh, command[0])
+            proc(*command[1:], _env=os.environ, _fg=True)
+
+        if args.write_tag:
+            write_tag(args)
+
+        if args.push:
+            docker_image = os.environ['DOCKER_IMAGE']
+
+            if args.dry_run:
+                logger.info(f'docker push {docker_image}')
+            else:
+                sh.docker('push', docker_image, _fg=True)
+
+    @property
+    def logger(self) -> logging.Logger:
+        return logging.getLogger(f'{__name__}.{self.__class__.__name__}')
