1ada41a8593aff8eb6126b6ae0c13eb7eb698813
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-05-14 11:46:38 -0400

    Add ErrorMessage and ProfileError

--- src/dc_workflows/errors.py
+++ src/dc_workflows/errors.py
@@ -3,10 +3,21 @@ class CommandError(Exception):
     Raised when a problem with the command run is encountered
     """
 
+class ErrorMessage(Exception):
+    """
+    Subclass to print out error message instead of entire stack trace
+    """
+
 
 class NoSuchProfile(Exception):
     """
     Raised when a requested profile is not listed in dc.yml
+    """
+
+
+class ProfileError(ErrorMessage):
+    """
+    Raised when there is a problem with a Profile
     """
 
 
