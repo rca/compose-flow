1589a38c5b457d4c04c346255d5be2d63efe5956
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-05-25 16:41:33 -0400

    Update long_description_content_type to text/plain

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     version='0.0.0',
     description='codified workflows for docker compose',
     long_description=readme(),
+    long_description_content_type='text/plain',
     package_dir={'':'src'},
     packages=packages,
     entry_points={
