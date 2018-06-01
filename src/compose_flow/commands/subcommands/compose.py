9f215d3e1d7a785139ca498eef4064e71c6b8dae
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-06-01 12:53:02 -0400

    Refactor out PassthroughBaseSubcommand

--- src/compose_flow/commands/subcommands/compose.py
+++ src/compose_flow/commands/subcommands/compose.py
@@ -3,58 +3,34 @@ Compose subcommand
 """
 import argparse
 import logging
-import os
-import sys
 
-import sh
+from .passthrough_base import PassthroughBaseSubcommand
 
-from .base import BaseSubcommand
-
-from compose_flow import docker
-
-from distutils.spawn import find_executable
+from compose_flow import docker, errors
 
 
-class Compose(BaseSubcommand):
+class Compose(PassthroughBaseSubcommand):
     """
     Subcommand for running compose commands
     """
+    command_name = 'docker-compose'
     dirty_working_copy_okay = True
 
-    def __init__(self, *args, **kwargs):
-        # pop off the compose_args kwarg
-        compose_args = kwargs.pop('compose_args', None)
+    def get_command(self):
+        command = super().get_command()
 
-        super().__init__(*args, **kwargs)
+        command.extend([
+            '--project-name', self.env.env_name,
+            '-f', self.profile.filename,
+        ])
 
-    @classmethod
-    def fill_subparser(cls, parser, subparser) -> None:
-        subparser.add_argument('compose_args', nargs=argparse.REMAINDER)
+        return command
 
-    def handle(self, compose_args:list=None) -> [None, str]:
+    def handle(self, extra_args: list=None) -> [None, str]:
         # check the profile to make sure it defines all the needed environment variables
         self.profile.check()
 
-        # check to make sure docker-compose is installed
-        docker_compose_path = find_executable('docker-compose')
-        if docker_compose_path is None:
-            return f'docker-compose not found in PATH; is it installed?'
-
-        command = [
-            docker_compose_path,
-            '--project-name', self.env.env_name,
-            '-f', self.profile.filename,
-        ]
-
-        compose_args = compose_args or self.args.compose_args
-        command.extend(compose_args)
-
-        self.logger.info(' '.join(command))
-
-        if not self.args.dry_run:
-            # os.execve(command[0], command, os.environ)
-            proc = getattr(sh, command[0])
-            proc(*command[1:], _env=os.environ, _fg=True)
+        super().handle(extra_args=extra_args)
 
     @property
     def logger(self) -> logging.Logger:
