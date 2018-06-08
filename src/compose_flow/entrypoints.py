ead91f6a6d57a6537db2f7b8bd419993572b4d34
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-06-08 09:25:19 -0400

    Add friendly error message when running Python<3.6

--- src/compose_flow/entrypoints.py
+++ src/compose_flow/entrypoints.py
@@ -6,6 +6,12 @@ Main console script entrypoints for the dc tool
 import logging
 import sys
 
+MIN_VERSION = (3, 6)
+RUNTIME_VERSION = (sys.version_info.major, sys.version_info.minor)
+
+if RUNTIME_VERSION < MIN_VERSION:
+    sys.exit('Error: compose-flow runs on Python3.6+')
+
 from .commands import ComposeFlow
 
 
