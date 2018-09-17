db6a0cd37d32f1ccc41dbfc8e625ba154a1f4a93
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-09-16 23:15:57 -0400

    add shell module

---
+++ src/compose_flow/shell.py
@@ -0,0 +1,19 @@
+import sh
+import shlex
+
+from sh import ErrorReturnCode_1
+
+
+def execute(command: str, env, **kwargs):
+    """
+    Executes a shell command
+    """
+    command_split = shlex.split(command)
+
+    kwargs.update(dict(
+        _env=env,
+    ))
+
+    proc = getattr(sh, command_split[0])
+
+    return proc(*command_split[1:], **kwargs)
