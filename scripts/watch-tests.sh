7fa2be10b67d0a01ab70027e136dece86c50ce60
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-09-17 01:00:32 -0400

    Run nose in verbose mode

--- scripts/watch-tests.sh
+++ scripts/watch-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-watchmedo shell-command --recursive --ignore-directories --drop --wait -c 'nosetests src/' src/
+watchmedo shell-command --recursive --ignore-directories --drop --wait -c 'nosetests -v src/' src/
