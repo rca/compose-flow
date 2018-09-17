5f309d5fedc4c8bef8dfc7269c48d402ff01fff5
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-09-17 09:10:15 -0400

    Remove DOCKER_HOST from the environment for tests
    It would suck to accidentally run stuff on unexpecting hosts.

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -9,6 +9,9 @@ os.environ['CF_DOCKER_IMAGE_PREFIX'] = CF_DOCKER_IMAGE_PREFIX
 
 class BaseTestCase(TestCase):
     def setUp(self):
+        # disconnect any remote docker hosts
+        os.environ.pop('DOCKER_HOST', None)
+
         super().setUp()
 
         self.sh_patcher = mock.patch('compose_flow.shell.sh')
