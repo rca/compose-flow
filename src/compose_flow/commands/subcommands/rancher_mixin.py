081572aac569ab21bca5a357949f35bd359cb60a
Marcus Levine <marcusianl@gmail.com>
2018-09-26 10:24:21 -0400

    add logic to render answers and manifests for rancher deployment

--- src/compose_flow/commands/subcommands/rancher_mixin.py
+++ src/compose_flow/commands/subcommands/rancher_mixin.py
@@ -1,12 +1,17 @@
 """
 Compose subcommand
 """
-from .passthrough_base import PassthroughBaseSubcommand
 import yaml
 import sh
 import os
+
+from functools import lru_cache
+
 from compose_flow.config import get_config
 from compose_flow import shell
+from compose_flow.utils import render, yaml_load, yaml_dump
+
+from .passthrough_base import PassthroughBaseSubcommand
 
 CLUSTER_LS_FORMAT = '{{.Cluster.Name}}: {{.Cluster.ID}}'
 PROJECT_LS_FORMAT = '{{.Project.Name}}: {{.Project.ID}}'
@@ -61,14 +66,16 @@ class RancherMixIn(object):
         depending on whether or not it is already deployed.
         '''
         apps = str(self.execute("rancher apps ls --format '{{.App.Name}}'"))
+        rendered_path = self.render_answers(app.answers, app.name)
         if app.name in apps:
-            return f'rancher apps upgrade --answers {app.answers} {app.name} {app.version}'
+            return f'rancher apps upgrade --answers {rendered_path} {app.name} {app.version}'
         else:
-            return f'rancher apps install --answers {app.answers} --namespace {app.namespace} --version {app.version} {app.chart} {app.name}'
+            return f'rancher apps install --answers {rendered_path} --namespace {app.namespace} --version {app.version} {app.chart} {app.name}'
 
     def get_manifest_deploy_command(self, manifest_path: str) -> str:
         '''Construct command to apply a Kubernetes YAML manifest using the Rancher CLI.'''
-        return f'rancher kubectl apply -f {manifest_path}'
+        rendered_path = self.render_manifest(manifest_path)
+        return f'rancher kubectl apply -f {rendered_path}'
 
     def get_apps(self) -> list:
         rancher_config = self.get_rancher_config_section()
@@ -77,3 +84,37 @@ class RancherMixIn(object):
     def get_manifests(self) -> list:
         rancher_config = self.get_rancher_config_section()
         return rancher_config.get('manifests', [])
+
+    def get_manifest_filename(self, manifest_path: str) -> str:
+        args = self.workflow.args
+        escaped_path = manifest_path.replace('/', '-')
+        return f'compose-flow-{args.profile}-manifest-{escaped_path}'
+
+    def get_answers_filename(self, app_name: str) -> str:
+        args = self.workflow.args
+        return f'compose-flow-{args.profile}-{app_name}-answers.yml'
+
+    def render_single_yaml(self, input_path: str, output_path: str) -> None:
+        '''Read in single YAML file from specified path, render environment variables, then write out to a known location in the working dir.'''
+        with open(input_path, 'r') as fh:
+            content = yaml_load(fh)
+        rendered = render(content, env=self.workflow.environment.data)
+
+        with open(output_path, 'w') as fh:
+            yaml_dump(rendered, fh)
+
+    @lru_cache
+    def render_manifest(self, manifest_path: str) -> str:
+        '''Render the specified manifest YAML and return the path to the rendered file.'''
+        rendered_path = self.get_manifest_filename(manifest_path)
+        self.render_single_yaml(manifest_path, rendered_path)
+
+        return rendered_path
+
+    @lru_cache
+    def render_answers(self, answers_path: str, app_name: str) -> str:
+        '''Render the specified manifest YAML and return the path to the rendered file.'''
+        rendered_path = self.get_answers_filename(app_name)
+        self.render_single_yaml(answers_path, rendered_path)
+
+        return rendered_path
