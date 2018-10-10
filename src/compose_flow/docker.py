ef5a6e13afbe0f71aa8bfb2d51ed0dcc8b6b39c8
Roberto    Aguilar <roberto.c.aguilar@gmail.com>
2018-10-10 13:50:57 -0400

    Refactor out get_docker_json()
    Create a single function that can act as a docker command executor and can parse JSON.

--- src/compose_flow/docker.py
+++ src/compose_flow/docker.py
@@ -4,7 +4,7 @@ import os
 
 from compose_flow import shell
 
-from .errors import NoSuchConfig, NotConnected
+from .errors import DockerError, NoSuchConfig, NotConnected
 
 
 def get_configs() -> list:
@@ -21,20 +21,15 @@ def get_config(name: str) -> str:
     Returns the content of the config in the swarm
     """
     try:
-        proc = shell.execute(f'docker config inspect {name}', os.environ)
-    except shell.ErrorReturnCode_1 as exc:
-        exc_s = f'{exc}'.lower()
+        data = get_docker_json(f'docker config inspect {name}', os.environ)
+    except DockerError as exc:
+        exc_s = str(exc)
 
         # if the config does not exist in docker, raise NoSuchConfig
         if 'no such config' in exc_s:
             raise NoSuchConfig(f'config name={name} not found')
-        elif 'cannot connect to the docker daemon' in exc_s:
-            raise NotConnected()
 
         raise
-
-    content = proc.stdout.decode('utf8')
-    data = json.loads(content)
 
     config_data = data[0]['Spec']['Data']
 
@@ -57,3 +52,42 @@ def remove_config(name: str) -> None:
     Removes a config from the swarm
     """
     shell.execute(f'docker config rm {name}', os.environ)
+
+
+def get_docker_json(command: str, env: dict) -> dict:
+    """
+    Returns docker output as a JSON object
+
+    Args:
+        command: the docker command to run
+        env: the environment to run the command with
+
+    Returns:
+        dict
+    """
+    content = get_docker_output(command, env)
+
+    return json.loads(content)
+
+
+def get_docker_output(command: str, env: dict) -> str:
+    """
+    Returns docker stdout as a string
+
+    Args:
+        command: the docker command to run
+        env: the environment to run the command under
+
+    Returns:
+        str
+    """
+    try:
+        proc = shell.execute(command, env)
+    except shell.ErrorReturnCode_1 as exc:
+        exc_s = f'{exc}'.lower()
+        if 'cannot connect to the docker daemon' in exc_s:
+            raise NotConnected()
+
+        raise DockerError(exc)
+
+    return proc.stdout.decode('utf8')
