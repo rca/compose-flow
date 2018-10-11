68f6a363ac74c54e631f8f5d06fe97a35eb924ba
Roberto    Aguilar <roberto.c.aguilar@gmail.com>
2018-10-11 08:59:37 -0400

    Promote some mocks to the TestCase

--- src/tests/test_deploy.py
+++ src/tests/test_deploy.py
@@ -3,26 +3,27 @@ import shlex
 from unittest import mock
 
 from compose_flow.commands import Workflow
+from compose_flow.commands.subcommands.profile import Profile
 
 from tests import BaseTestCase
 
 
+@mock.patch('compose_flow.commands.subcommands.env.docker')
 @mock.patch('compose_flow.commands.subcommands.env.os')
+@mock.patch('compose_flow.commands.subcommands.profile.Profile.write')
 class DeployTestCase(BaseTestCase):
-    @mock.patch('compose_flow.commands.subcommands.profile.Profile.write')
     @mock.patch('compose_flow.commands.subcommands.env.Env.rw_env', new=True)
     @mock.patch('compose_flow.commands.subcommands.env.utils')
-    @mock.patch('compose_flow.commands.subcommands.env.docker')
     def test_version_written(self, *mocks):
         """
         Ensures that version in env is written to the docker config
         """
-        docker_mock = mocks[0]
+        docker_mock = mocks[-1]
         docker_mock.get_config.return_value = (
             f"FOO=1\nBAR=2\nVERSION=1.0\nDOCKER_IMAGE=foo:dev"
         )
 
-        utils_mock = mocks[1]
+        utils_mock = mocks[0]
         utils_mock.get_tag_version.return_value = '0.0.1-test'
         utils_mock.render.side_effect = lambda x, **kwargs: x
 
