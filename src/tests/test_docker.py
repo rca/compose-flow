8f67a5e02c11c1eca2f57e6823cd2268d2212152
Roberto    Aguilar <roberto.c.aguilar@gmail.com>
2018-10-12 11:03:45 -0400

    Add test

--- src/tests/test_docker.py
+++ src/tests/test_docker.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, mock
 
 from compose_flow import docker
+from compose_flow.errors import DockerError, NoSuchConfig
 
 
 class DockerTestCase(TestCase):
@@ -10,3 +11,13 @@ class DockerTestCase(TestCase):
         """
         with docker.json_formatter('docker node ls') as json_command:
             self.assertEqual('docker node ls --format "{{ json . }}"', json_command)
+
+    @mock.patch('compose_flow.docker.get_docker_json')
+    def test_get_config_no_such_config(self, *mocks):
+        """
+        Ensure NoSuchConfig is raised
+        """
+        get_docker_json_mock = mocks[0]
+        get_docker_json_mock.side_effect = DockerError('No such config')
+
+        self.assertRaises(NoSuchConfig, docker.get_config, 'test')
