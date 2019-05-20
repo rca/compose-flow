a7c30bef2c69563253b0e2ac4459663f4164a159
Jonathan Meier <jonathanwmeier@gmail.com>
2019-05-20 18:00:00 -0400

    Integrate with publish subcommand

--- src/compose_flow/commands/subcommands/publish.py
+++ src/compose_flow/commands/subcommands/publish.py
@@ -1,3 +1,7 @@
+import argparse
+from typing import List
+
+from compose_flow.image import PrivateImage
 
 from .base import BaseBuildSubcommand
 
@@ -7,7 +11,19 @@ class Publish(BaseBuildSubcommand):
     Subcommand for building and pushing Docker images
     """
 
-    def get_built_docker_images(self) -> list:
+    @classmethod
+    def fill_subparser(cls, parser, subparser):
+        subparser.epilog = __doc__
+        subparser.formatter_class = argparse.RawDescriptionHelpFormatter
+
+        subparser.add_argument(
+            '--auto-tag',
+            default=False,
+            action='store_true',
+            help='automatically publish major and major.minor tags pointing to the new docker image',
+        )
+
+    def get_built_docker_images(self) -> List[PrivateImage]:
         """
         Returns a list of docker images built in the compose file
         """
@@ -16,9 +32,16 @@ class Publish(BaseBuildSubcommand):
         profile = self.workflow.profile
         for service_data in profile.data['services'].values():
             if service_data.get('build'):
-                docker_images.add(service_data.get('image'))
-
-        return list(docker_images)
+                tagged_image_name = service_data.get('image')
+                docker_images.add(tagged_image_name)
+        return [
+            PrivateImage(
+                tagged_image_name=tagged_image_name,
+                publish_callable=self.execute_publish,
+                tag_callable=self.execute_tag,
+            )
+            for tagged_image_name in docker_images
+        ]
 
     def push(self):
         docker_images = self.get_built_docker_images()
@@ -28,11 +51,16 @@ class Publish(BaseBuildSubcommand):
 
             if self.workflow.args.dry_run:
                 self.logger.info(f'docker push {docker_image}')
+            elif self.workflow.args.auto_tags:
+                docker_image.publish_with_auto_tags()
             else:
-                self.execute_publish(docker_image)
+                docker_image.publish()
 
     def execute_publish(self, tagged_image_name: str):
         self.execute(f'docker push {tagged_image_name}', _fg=True)
+
+    def execute_tag(self, original, new):
+        self.execute(f'docker tag {original} {new}')
 
     def handle(self):
         self.build(pull=False)
