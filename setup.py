4b141ebe169fb6580630419abe4b50fc5a5f0afb
Roberto Aguilar <r@rreboto.com>
2019-10-15 07:34:41 -0400

    Update setup.py to get package versions from Pipfile

--- setup.py
+++ setup.py
@@ -15,17 +15,43 @@ def readme():
 def get_required_packages():
     """
     Returns the packages used for install_requires
+
     This used to pin down every package in Pipfile.lock to the version, but that, in turn, broke
     downstream projects because it was way too strict.
+
     Now, this simply grabs all the items listed in the `Pipfile` `[packages]` section without version
     pinning
     """
-    install_requires = []
-
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(strict=False)
     config.read("Pipfile")
 
-    install_requires = sorted([x for x in config["packages"]])
+    # when the Pipfile specifies a package, constrain it to that version
+    install_requires = []
+    for package, version in config["packages"].items():
+        full_package = package
+        package_version = None
+
+        if version[0] == "{":
+            uncurled = version[1:-1]
+            uncurled_split = [x.strip() for x in uncurled.split(",")]
+
+            # find the version
+            for item in uncurled_split:
+                if "version" in item:
+                    item_split = [x.strip() for x in item.split("=", 2)]
+
+                    version = item_split[1][1:-1]
+                    if version != "*":
+                        package_version = version
+
+                    break
+        elif version != '"*"':
+            package_version = version[1:-1]
+
+        if package_version:
+            full_package = f"{package}{package_version}"
+
+        install_requires.append(full_package)
 
     return install_requires
 
